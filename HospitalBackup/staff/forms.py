d55ccb2839ac23fe139e9888990fd3b9b24f7b5a
Pacifique5 <pacifiquemugisha040@gmail.com>
2025-12-01 12:08:40 +0200

    hospital backup system added

---
+++ HospitalBackup/staff/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Staff
+
+
+class StaffForm(forms.ModelForm):
+    class Meta:
+        model = Staff
+        fields = ['first_name', 'last_name', 'role', 'phone', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            if isinstance(field.widget, (forms.Select, forms.SelectMultiple)):
+                css_class = 'form-select'
+            else:
+                css_class = 'form-control'
+            existing = field.widget.attrs.get('class', '')
+            field.widget.attrs['class'] = (existing + ' ' + css_class).strip()
+            # Add sensible placeholders for better UX
+            field.widget.attrs.setdefault('placeholder', field.label)
+
+
